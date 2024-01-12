aab0217344888620eef192edef82011343578542
salimjonovsamandar <salimjonovsamandarbek777@gmail.com>
2024-01-12 23:28:19 +0500

    Vazifa bajarildi

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const button = document.getElementById("button");
+const input = document.getElementById("input");
+const cerad = document.getElementById("cerad");
+
+button.addEventListener("click", (e) => {});
+
+// const block = document.getElementById("block");
+
+// function craete() {
+//   return `
+// <img src="" alt="">
+// <p>${title.vale}</p>
+// `;
+// }
+
+// fetch("https://jsonplaceholder.typicode.com/photos")
+//   .then((data) => data.json())
+//   .then((res) => {
+//     craete();
+//     console.log(res);
+//   });
+
+// https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=96b947a45d33d7dc1c49af3203966408
