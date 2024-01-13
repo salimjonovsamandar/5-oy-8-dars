1e42b686cf9e5615dfb082b67f22b38fbd3770e7
salimjonovsamandar <salimjonovsamandarbek777@gmail.com>
2024-01-13 11:29:05 +0500

    Vazifa bajarildi

--- js/index.js
+++ js/index.js
@@ -2,22 +2,44 @@ const button = document.getElementById("button");
 const input = document.getElementById("input");
 const cerad = document.getElementById("cerad");
 
-button.addEventListener("click", (e) => {});
+const KEY = "96b947a45d33d7dc1c49af3203966408";
 
-// const block = document.getElementById("block");
+function validate() {
+  if (!input.value.trim()) {
+    alert("SHaxar nomini kiriting");
+    return false;
+  }
+  if (Number(input.value)) {
+    alert("Shaxar nomi raqamda bo'lishi mumkin emas");
+    return false;
+  }
+  return true;
+}
 
-// function craete() {
-//   return `
-// <img src="" alt="">
-// <p>${title.vale}</p>
-// `;
-// }
+const getData = async (sity) => {
+  const base = "https://api.openweathermap.org/data/2.5/weather";
+  const puery = `?q=${sity}&units=metric&appid=${KEY}`;
 
-// fetch("https://jsonplaceholder.typicode.com/photos")
-//   .then((data) => data.json())
-//   .then((res) => {
-//     craete();
-//     console.log(res);
-//   });
+  const req = await fetch(base + puery);
+  const data = await req.json();
 
-// https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=96b947a45d33d7dc1c49af3203966408
+  return data;
+};
+
+function creatCard(weather) {
+  document.querySelector(".nom").innerHTML = weather.name;
+  document.querySelector(".grad").innerHTML = weather.main.temp;
+ }
+
+function clear() {
+  input.value = "";
+}
+
+button.addEventListener("click", () => {
+  validate();
+
+  getData(input.value).then((data) => {
+    creatCard(data);
+  });
+  clear();
+});
